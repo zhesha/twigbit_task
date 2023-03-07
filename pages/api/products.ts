05457e274b60337820f457ea0b85d6e5fd438176
zhesha <savzhe@gmail.com>
2023-03-07 14:29:12 +0200

    created product api endpoint

---
+++ pages/api/products.ts
@@ -0,0 +1,14 @@
+import product_mock from "./product_mock.json";
+
+interface Product {
+  id?: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+// TODO get rid of `any` type
+export default function handler(req: any, res: any) {
+  product_mock.items.forEach((item: Product, index: number) => item.id = index);
+  res.status(200).json(product_mock);
+}
